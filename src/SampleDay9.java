cffba59e0422db110fb2f9ed73e9778e46523089
SuyashPal54325 <suyashpal54325@gmail.com>
2024-09-03 11:44:40 +0530

    Understanding of time and space complexity

---
+++ src/SampleDay9.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class SampleDay9 {
+    public static void main(String args[]){
+
+        // TIME COMPLEXITY IS 'n'
+//        Scanner sc=new Scanner(System.in);
+//        System.out.println("Enter the value of n:");
+//        int n=sc.nextInt();
+//        for (int i=1;i<=n;i++){
+//            System.out.println("Hello");
+//        }
+
+
+        // TIME COMPLEXITY IS 'n*m'
+//        Scanner sc=new Scanner(System.in);
+//        int n=sc.nextInt();
+//        int m=sc.nextInt();
+//        for (int i=1;i<=n;i++){
+//            for (int j=1;j<=m;j++){
+//                System.out.println("Hello");
+//            }
+//         }
+
+
+        // TIME COMPLEXITY IS 'n*n'
+//        Scanner sc=new Scanner(System.in);
+//        System.out.println("Enter the value of n:");
+//        int n=sc.nextInt();
+//        for (int i=1;i<=n;i++){
+//            for (int j=1;j<=n;j++) {
+//                System.out.println("Hello");
+//            }
+//        }
+
+
+        // TIME COMPLEXITY IS 'n+m'
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        for (int i=1;i<=n;i++){
+                System.out.println("Hello");
+         }
+        for (int j=1;j<=m;j++){
+            System.out.println("Hello");
+        }
+
+
+        }
+}
